92de27db091f27d254d1673d2bf46510b86be17b
mushymato <wildshinobu@gmail.com>
2024-06-13 20:07:39 -0400

    initial commit

---
+++ SprinklerAttachments/ModEntry.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace SprinklerAttachments
+{
+    /// <summary>The mod entry point.</summary>
+    internal sealed class ModEntry : Mod
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            // helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+            helper.Events.World.ChestInventoryChanged += this.OnChestInventoryChanged;
+        }
+
+
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        private void OnChestInventoryChanged(object? sender, ChestInventoryChangedEventArgs e)
+        {
+            // ignore if player hasn't loaded a save yet
+            if (!Context.IsWorldReady)
+                return;
+
+            // print button presses to the console window
+            this.Monitor.Log($"ChestInventoryChanged:\n\tChest={e.Chest}\n\tLocation={e.Location}\n\t{e.Added}\n\t{e.Removed}\n\t{e.QuantityChanged}", LogLevel.Info);
+        }
+    }
+}
