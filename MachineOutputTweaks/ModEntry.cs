4371c088888d366b678d5cb109ec54efe37cbd3c
mushymato <wildshinobu@gmail.com>
2024-07-11 22:54:42 -0400

    rename a mod, update readme

---
+++ MachineOutputTweaks/ModEntry.cs
@@ -0,0 +1,90 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.GameData;
+using StardewValley.GameData.Machines;
+using StardewValley.ItemTypeDefinitions;
+
+namespace MachineOutputTweaks
+{
+    public class ModEntry : Mod
+    {
+        private static readonly string[] ExcludedMachines = {
+            "(BC)20", // recycling machine
+        };
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Content.AssetRequested += OnAssetRequested;
+        }
+
+        private void OnAssetRequested(object? sender, AssetRequestedEventArgs e)
+        {
+            if (e.Name.IsEquivalentTo("Data/Machines"))
+            {
+                e.Edit(EditDataMachines, AssetEditPriority.Late);
+            }
+        }
+
+        private void EditDataMachines(IAssetData asset)
+        {
+            IDictionary<string, MachineData> data = asset.AsDictionary<string, MachineData>().Data;
+            foreach (KeyValuePair<string, MachineData> kv in data)
+            {
+                string qItemId = kv.Key;
+                MachineData machine = kv.Value;
+                if (machine.IsIncubator || machine.OutputRules == null || !machine.AllowFairyDust || ExcludedMachines.Contains(qItemId))
+                    continue;
+
+                foreach (MachineOutputRule rule in machine.OutputRules)
+                {
+                    if (rule.OutputItem == null)
+                        continue;
+                    if (rule.Triggers.Any((trig) => trig.Trigger != MachineOutputTrigger.ItemPlacedInMachine))
+                        continue;
+                    rule.OutputItem.ForEach(item =>
+                    {
+                        if (item is null || item.OutputMethod != null || ItemRegistry.GetDataOrErrorItem(item.ItemId) is not ParsedItemData itemData)
+                            return;
+                        if (itemData.Category == StardewValley.Object.artisanGoodsCategory) // keep quality artisan recipes, 
+                        {
+                            if (item.Quality == 2)
+                            { // special case large milk/egg, copy quality, but produce 2
+                                item.StackModifiers ??= new List<QuantityModifier>();
+                                item.StackModifiers.Add(new()
+                                {
+                                    Modification = QuantityModifier.ModificationType.Add,
+                                    Amount = 1
+                                });
+                                item.Quality = -1;
+                            }
+                            item.CopyQuality = true;
+                            Monitor.Log($"Machine rule");
+                        }
+                        else // increase output depending on input quality
+                        {
+                            item.StackModifiers ??= new List<QuantityModifier>();
+                            item.StackModifiers.Add(new()
+                            {
+                                Condition = "ITEM_QUALITY Input 1 1",
+                                Modification = QuantityModifier.ModificationType.Add,
+                                Amount = 1
+                            });
+                            item.StackModifiers.Add(new()
+                            {
+                                Condition = "ITEM_QUALITY Input 2 2",
+                                Modification = QuantityModifier.ModificationType.Add,
+                                Amount = 2
+                            });
+                            item.StackModifiers.Add(new()
+                            {
+                                Condition = "ITEM_QUALITY Input 4 4",
+                                Modification = QuantityModifier.ModificationType.Add,
+                                Amount = 3
+                            });
+                        }
+                    });
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,13 +1,27 @@
 Stardew Valley Mods
 
-#### SprinklerAttachments + [CP] SprinklerAttachments
+## Released (available on Nexus)
+
+#### [Sprinkler Attachments](https://www.nexusmods.com/stardewvalley/mods/25326)
 Adds new items that can be placed on sprinklers to plant seeds and apply fertilizer.
 
 
-#### Full Inventory Toolbar
+#### [Full Inventory Toolbar](https://www.nexusmods.com/stardewvalley/mods/25711)
 Changes the toolbar. Now you can have all 36 items on your screen at once.
 
 
-#### CompatibleArtisanGoodsKeepQuality
-Quick n' dirty mod that patches machine recipes to respect quality of input with artisan items, so that iridium produces gold and gold produces silver (silver produces normal, no change).
-Done in C# to apply changes to mods easily, but strange things may happen since the check is not well tested.
+#### [Special Order Notifications](https://www.nexusmods.com/stardewvalley/mods/25999)
+Extend the daily quest notifications from the vanilla game to also affect special orders.
+
+
+## Unreleased
+
+#### Machine Output Tweaks
+Tweaks output of various artisan machines so that they respect quality of the input item, done via C# to handle modded content better.
+Mostly just a personal version of [Artisan Goods Keep Quality](https://www.nexusmods.com/stardewvalley/mods/21278)
+
+#### Custom Building Entry
+Allow building/map modders to specify where the player should spawn when entering a farm building through map property BuildingEntryLocation <x> <y>
+
+#### Custom Introductions
+Allow NPC modders to create "introduction" type quests for an arbiturary group of NPC.
