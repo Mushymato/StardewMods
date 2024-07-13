6d48458be55f693accb8e5fec6c565c9be37b692
mushymato <wildshinobu@gmail.com>
2024-07-12 23:40:19 -0400

    more visible cask rarity

---
+++ MoreVisibleCaskRarity/ModEntry.cs
@@ -0,0 +1,93 @@
+﻿using System.Reflection.Emit;
+using Microsoft.Xna.Framework;
+using HarmonyLib;
+using StardewModdingAPI;
+using StardewValley;
+using StardewValley.Objects;
+
+
+namespace MoreVisibleCaskRarity
+{
+    public class ModConfig
+    {
+        public int OffsetX { get; set; } = 0;
+        public int OffsetY { get; set; } = -50;
+    }
+    public class ModEntry : Mod
+    {
+        private static IMonitor? mon;
+        private static ModConfig? config;
+
+        public override void Entry(IModHelper helper)
+        {
+            mon = Monitor;
+            config = Helper.ReadConfig<ModConfig>();
+            Helper.WriteConfig(config);
+            Patch();
+        }
+
+        public static void Log(string msg, LogLevel level = LogLevel.Debug)
+        {
+            mon!.Log(msg, level);
+        }
+
+        private void Patch()
+        {
+            Harmony harmony = new(ModManifest.UniqueID);
+            try
+            {
+                harmony.Patch(
+                    original: AccessTools.DeclaredMethod(typeof(Cask), nameof(Cask.draw)),
+                    transpiler: new HarmonyMethod(AccessTools.DeclaredMethod(typeof(ModEntry), nameof(Cask_draw_Transpiler)))
+                );
+            }
+            catch (Exception err)
+            {
+                Log($"Failed to patch MoreVisibleCaskRarity:\n{err}", LogLevel.Error);
+            }
+        }
+
+        private static IEnumerable<CodeInstruction> Cask_draw_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            try
+            {
+                CodeMatcher matcher = new(instructions, generator);
+
+                matcher.Start()
+                .MatchEndForward(new CodeMatch[]{
+                    new(OpCodes.Ldsfld, AccessTools.Field(typeof(Game1), nameof(Game1.viewport))),
+                    new(OpCodes.Ldarg_2),
+                    new(OpCodes.Ldc_I4_S, (sbyte)64),
+                    new(OpCodes.Mul),
+                    new(OpCodes.Conv_R4)
+                })
+                .InsertAndAdvance(new CodeInstruction[]{
+                    // new(OpCodes.Call, AccessTools.DeclaredMethod(typeof(ModEntry), nameof(GetOffsetX))),
+                    new(OpCodes.Ldc_I4, config!.OffsetX),
+                    new(OpCodes.Add)
+                })
+                .MatchEndForward(new CodeMatch[]{
+                    new(OpCodes.Ldarg_3),
+                    new(OpCodes.Ldc_I4_S, (sbyte)64),
+                    new(OpCodes.Mul),
+                    new(OpCodes.Ldc_I4_S, (sbyte)64),
+                    new(OpCodes.Sub),
+                    new(OpCodes.Conv_R4)
+                })
+                .InsertAndAdvance(new CodeInstruction[]{
+                    // new(OpCodes.Call, AccessTools.DeclaredMethod(typeof(ModEntry), nameof(GetOffsetY))),
+                    new(OpCodes.Ldc_I4, config!.OffsetY),
+                    new(OpCodes.Add)
+                })
+                ;
+
+                return matcher.Instructions();
+            }
+            catch (Exception err)
+            {
+                Log($"Error in Cask_draw_Transpiler:\n{err}", LogLevel.Error);
+                return instructions;
+            }
+        }
+    }
+}
