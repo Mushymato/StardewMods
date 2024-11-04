a37eb2d1ac959e3cc66f1a9d9770fe8985867664
mushymato <wildshinobu@gmail.com>
2024-11-04 10:49:06 -0500

    chagne templates

--- csharp-templates/sdvmod-project/ModEntry.cs
+++ csharp-templates/sdvmod-project/ModEntry.cs
@@ -2,47 +2,46 @@ using MODNAME.Framework;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 
-namespace MODNAME
+namespace MODNAME;
+
+public class ModEntry : Mod
 {
-    public class ModEntry : Mod
-    {
 //-:cnd:noEmit
 #if DEBUG
-        private const LogLevel DEFAULT_LOG_LEVEL = LogLevel.Debug;
+    private const LogLevel DEFAULT_LOG_LEVEL = LogLevel.Debug;
 #else
-        private const LogLevel DEFAULT_LOG_LEVEL = LogLevel.Trace;
+    private const LogLevel DEFAULT_LOG_LEVEL = LogLevel.Trace;
 #endif
 //+:cnd:noEmit
-        private static IMonitor? mon;
-        internal static ModConfig Config = null!;
+    private static IMonitor? mon;
+    internal static ModConfig Config = null!;
 
-        public override void Entry(IModHelper helper)
-        {
-            I18n.Init(helper.Translation);
-            mon = Monitor;
-            Config = Helper.ReadConfig<ModConfig>();
-            helper.Events.GameLoop.GameLaunched += OnGameLaunched;
-        }
+    public override void Entry(IModHelper helper)
+    {
+        I18n.Init(helper.Translation);
+        mon = Monitor;
+        Config = Helper.ReadConfig<ModConfig>();
+        helper.Events.GameLoop.GameLaunched += OnGameLaunched;
+    }
 
-        private void OnGameLaunched(object? sender, GameLaunchedEventArgs e)
-        {
-            Config.Register(Helper, ModManifest);
-        }
+    private void OnGameLaunched(object? sender, GameLaunchedEventArgs e)
+    {
+        Config.Register(Helper, ModManifest);
+    }
 
-        /// <summary>SMAPI static monitor Log wrapper</summary>
-        /// <param name="msg"></param>
-        /// <param name="level"></param>
-        internal static void Log(string msg, LogLevel level = DEFAULT_LOG_LEVEL)
-        {
-            mon!.Log(msg, level);
-        }
+    /// <summary>SMAPI static monitor Log wrapper</summary>
+    /// <param name="msg"></param>
+    /// <param name="level"></param>
+    internal static void Log(string msg, LogLevel level = DEFAULT_LOG_LEVEL)
+    {
+        mon!.Log(msg, level);
+    }
 
-        /// <summary>SMAPI static monitor LogOnce wrapper</summary>
-        /// <param name="msg"></param>
-        /// <param name="level"></param>
-        internal static void LogOnce(string msg, LogLevel level = DEFAULT_LOG_LEVEL)
-        {
-            mon!.LogOnce(msg, level);
-        }
+    /// <summary>SMAPI static monitor LogOnce wrapper</summary>
+    /// <param name="msg"></param>
+    /// <param name="level"></param>
+    internal static void LogOnce(string msg, LogLevel level = DEFAULT_LOG_LEVEL)
+    {
+        mon!.LogOnce(msg, level);
     }
 }
--- csharp-templates/sdvmod/MODNAME/Framework/ModConfig.cs
+++ csharp-templates/sdvmod/MODNAME/Framework/ModConfig.cs
@@ -1,32 +1,31 @@
 using StardewModdingAPI;
 
-namespace MODNAME.Framework
+namespace MODNAME.Framework;
+
+internal sealed class ModConfig
 {
-    internal sealed class ModConfig
+    /// <summary>Restore default config values</summary>
+    private void Reset()
     {
-        /// <summary>Restore default config values</summary>
-        private void Reset()
-        {
 
-        }
+    }
 
-        /// <summary>Add mod config to GMCM if available</summary>
-        /// <param name="helper"></param>
-        /// <param name="mod"></param>
-        public void Register(IModHelper helper, IManifest mod)
+    /// <summary>Add mod config to GMCM if available</summary>
+    /// <param name="helper"></param>
+    /// <param name="mod"></param>
+    public void Register(IModHelper helper, IManifest mod)
+    {
+        Integration.IGenericModConfigMenuApi? GMCM = helper.ModRegistry.GetApi<Integration.IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+        if (GMCM == null)
         {
-            Integration.IGenericModConfigMenuApi? GMCM = helper.ModRegistry.GetApi<Integration.IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
-            if (GMCM == null)
-            {
-                helper.WriteConfig(this);
-                return;
-            }
-            GMCM.Register(
-                mod: mod,
-                reset: () => { Reset(); helper.WriteConfig(this); },
-                save: () => { helper.WriteConfig(this); },
-                titleScreenOnly: false
-            );
+            helper.WriteConfig(this);
+            return;
         }
+        GMCM.Register(
+            mod: mod,
+            reset: () => { Reset(); helper.WriteConfig(this); },
+            save: () => { helper.WriteConfig(this); },
+            titleScreenOnly: false
+        );
     }
 }
