521ce8fff634c0e90363b8770e7b152051c0415f
mushymato <wildshinobu@gmail.com>
2025-06-17 21:36:41 -0400

    carpenter autobuy mod

--- CarpenterAutobuy/ModEntry.cs
+++ CarpenterAutobuy/ModEntry.cs
@@ -1,38 +1,82 @@
+using System.Runtime.CompilerServices;
+using HarmonyLib;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using StardewValley.Menus;
 
 namespace CarpenterAutobuy;
 
 public sealed class ModEntry : Mod
 {
-#if DEBUG
-    private const LogLevel DEFAULT_LOG_LEVEL = LogLevel.Debug;
-#else
-    private const LogLevel DEFAULT_LOG_LEVEL = LogLevel.Trace;
-#endif
-
     public const string ModId = "mushymato.CarpenterAutobuy";
-    private static IMonitor? mon;
+
+    private sealed record PriceModContext(int Price, List<(int, Item)> Originals);
+
+    private static readonly ConditionalWeakTable<CarpenterMenu.BlueprintEntry, PriceModContext> ExtraCosts = [];
+    private ModConfig Config = null!;
+
+    private class ModConfig
+    {
+        internal readonly KeybindList Key = KeybindList.Parse("Tab");
+    }
 
     public override void Entry(IModHelper helper)
     {
         I18n.Init(helper.Translation);
-        mon = Monitor;
+        helper.Events.Input.ButtonsChanged += OnButtonsChanged;
+        Config = Helper.ReadConfig<ModConfig>();
+        Harmony harmony = new(ModId);
+        harmony.Patch(
+            original: AccessTools.DeclaredPropertyGetter(
+                typeof(CarpenterMenu.BlueprintEntry),
+                nameof(CarpenterMenu.BlueprintEntry.BuildCost)
+            ),
+            postfix: new HarmonyMethod(typeof(ModEntry), nameof(BlueprintEntry_BuildCost_Postfix))
+        );
     }
 
-    /// <summary>SMAPI static monitor Log wrapper</summary>
-    /// <param name="msg"></param>
-    /// <param name="level"></param>
-    internal static void Log(string msg, LogLevel level = DEFAULT_LOG_LEVEL)
+    private void OnButtonsChanged(object? sender, ButtonsChangedEventArgs e)
     {
-        mon!.Log(msg, level);
+        if (!Config.Key.JustPressed() || Game1.activeClickableMenu is not CarpenterMenu carpenterMenu)
+            return;
+        if (ExtraCosts.TryGetValue(carpenterMenu.Blueprint, out PriceModContext? ctx))
+        {
+            foreach ((int i, Item item) in ctx.Originals)
+            {
+                carpenterMenu.ingredients.Insert(i, item);
+            }
+            ExtraCosts.Remove(carpenterMenu.Blueprint);
+        }
+        else
+        {
+            List<(int, Item)> originals = [];
+            // (O)388 wood
+            // (O)390 stone
+            int extraCost = 0;
+            for (int i = 0; i < carpenterMenu.ingredients.Count; i++)
+            {
+                Item ingredient = carpenterMenu.ingredients[i];
+                if (ingredient.QualifiedItemId == "(O)388" || ingredient.QualifiedItemId == "(O)390")
+                {
+                    extraCost += ingredient.Stack * ingredient.salePrice();
+                    originals.Add(new(i, ingredient));
+                }
+            }
+            foreach ((int i, _) in originals.AsEnumerable().Reverse())
+            {
+                carpenterMenu.ingredients.RemoveAt(i);
+            }
+            ExtraCosts.Add(carpenterMenu.Blueprint, new(extraCost, originals));
+        }
     }
 
-    /// <summary>SMAPI static monitor LogOnce wrapper</summary>
-    /// <param name="msg"></param>
-    /// <param name="level"></param>
-    internal static void LogOnce(string msg, LogLevel level = DEFAULT_LOG_LEVEL)
+    private static void BlueprintEntry_BuildCost_Postfix(CarpenterMenu.BlueprintEntry __instance, ref int __result)
     {
-        mon!.LogOnce(msg, level);
+        if (ExtraCosts.TryGetValue(__instance, out PriceModContext? ctx))
+        {
+            __result += ctx.Price;
+        }
     }
 }
