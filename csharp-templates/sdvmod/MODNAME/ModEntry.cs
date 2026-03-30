1ad196e931a4bc30d870a3fd53b06ec45c0b8625
mushymato <wildshinobu@gmail.com>
2026-03-29 23:30:38 -0400

    more template fiddly

--- csharp-templates/sdvmod/MODNAME/ModEntry.cs
+++ csharp-templates/sdvmod/MODNAME/ModEntry.cs
@@ -1,6 +1,5 @@
 using System.Diagnostics;
 using StardewModdingAPI;
-using StardewModdingAPI.Events;
 
 namespace MODNAME;
 
@@ -15,12 +14,14 @@ public sealed class ModEntry : Mod
     //+:cnd:noEmit
 
     public const string ModId = "mushymato.MODNAME";
-    private static IMonitor? mon;
+    private static IMonitor mon = null!;
+    internal static IModHelper help = null!;
 
     public override void Entry(IModHelper helper)
     {
         I18n.Init(helper.Translation);
         mon = Monitor;
+        help = helper;
     }
 
     /// <summary>SMAPI static monitor Log wrapper</summary>
@@ -28,7 +29,7 @@ public sealed class ModEntry : Mod
     /// <param name="level"></param>
     internal static void Log(string msg, LogLevel level = DEFAULT_LOG_LEVEL)
     {
-        mon!.Log(msg, level);
+        mon.Log(msg, level);
     }
 
     /// <summary>SMAPI static monitor LogOnce wrapper</summary>
@@ -36,7 +37,7 @@ public sealed class ModEntry : Mod
     /// <param name="level"></param>
     internal static void LogOnce(string msg, LogLevel level = DEFAULT_LOG_LEVEL)
     {
-        mon!.LogOnce(msg, level);
+        mon.LogOnce(msg, level);
     }
 
     /// <summary>SMAPI static monitor Log wrapper, debug only</summary>
@@ -45,6 +46,6 @@ public sealed class ModEntry : Mod
     [Conditional("DEBUG")]
     internal static void LogDebug(string msg, LogLevel level = DEFAULT_LOG_LEVEL)
     {
-        mon!.Log(msg, level);
+        mon.Log(msg, level);
     }
 }
